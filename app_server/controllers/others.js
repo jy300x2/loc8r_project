05610787865ba1f5e568cfe6edd9bb83f385ad6a
Jon Yu <jy300x2@gmail.com>
2016-10-22 15:02:39 -0400

    Modified views and controllers. Added data to db.

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res) {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', { 
+    title: 'About Loc8r',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac cursus nulla. Curabitur scelerisque interdum erat, ac posuere velit vestibulum eu. Quisque imperdiet nec ex id tincidunt.'
+  });
 };

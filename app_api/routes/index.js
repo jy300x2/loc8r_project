511df46f08e81a057db382d65ad7ab68627216d3
Jon Yu <jy300x2@gmail.com>
2016-10-25 20:46:33 -0400

    Edited files to remove errors

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
-var ctrlOthers = require('../controllers/others');
+var ctrlReviews = require('../controllers/reviews');
 
-// locations
 router.get('/locations', ctrlLocations.locationsListByDistance);
 router.post('/locations', ctrlLocations.locationsCreate);
 router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
@@ -11,7 +10,7 @@ router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
 router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
 
 // reviews
-router.post('/locations/:locationid/reviews', ctrlReviews.reviewCreate);
+router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
 router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
 router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
 router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
